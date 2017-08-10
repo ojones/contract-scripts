d0805692f00049d7cc10f0f920a19f97137662a6
ojones <wakeupoj@gmail.com>
2017-08-10 12:45:11 -0700

    added modifiers

--- quick_analyze.sh
+++ quick_analyze.sh
@@ -8,13 +8,18 @@
 FILE=$1
 
 printf "\n"
-printf "Quick analysis of contract code...\n\n"
+printf "Quick analysis of $FILE contract code...\n\n"
 
 printf "List line counts of each found struct:\n"
 
-awk '/^ *struct/ { print $0 }' $FILE | sed 's/^.*struct//' | sed 's/[{].*$//' | sed '/^[[:blank:]]*$/d' | while read -r line; do  echo $(cat $FILE | grep -c "$line") $(echo $line); done | sort -nr
+awk '/^ *struct/ { print $0 }' $FILE | sed 's/^.*struct//' | sed 's/[{].*$//' | sed '/^[[:blank:]]*$/d' | while read -r line; do  echo "  "$(cat $FILE | grep -c "$line") $(echo $line); done | sort -nr
 
 printf "\n"
 printf "List line counts of each found function:\n"
 
-awk '/^ *function/ { print $0 }' $FILE | sed 's/^.*function//' | sed 's/[(].*$//' | sed '/^[[:blank:]]*$/d' | while read -r line; do echo $(cat $FILE | grep -c "$line") $(echo $line); done | sort -nr
+awk '/^ *function/ { print $0 }' $FILE | sed 's/^.*function//' | sed 's/[(].*$//' | sed '/^[[:blank:]]*$/d' | while read -r line; do echo "  "$(cat $FILE | grep -c "$line") $(echo $line); done | sort -nr
+
+printf "\n"
+printf "List line counts of each found modifier:\n"
+
+awk '/^ *modifier/ { print $0 }' $FILE | sed 's/^.*modifier//' | sed 's/[(].*$//' | sed 's/[{}].*$//' | sed '/^[[:blank:]]*$/d' | while read -r line; do echo "  "$(cat $FILE | grep -c "$line") $(echo $line); done | sort -nr
